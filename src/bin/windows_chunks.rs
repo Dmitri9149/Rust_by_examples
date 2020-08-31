50fff587450cc43fe0e8a802afcf2af41c871ef2
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-31 18:52:26 +0300

    file renamed

---
+++ src/bin/windows_chunks.rs
@@ -0,0 +1,16 @@
+// slice of window the window has type &[{integer}]
+
+fn main() {
+    let ints = [1, 2, 3, 4, 5,6];
+    let slice = &ints;
+
+    for s in slice.windows(2) {
+        println!("window {:?}", s);
+// check the type of the iter 
+//         let () = s
+    }
+
+    for s in slice.chunks(2) {
+        println!("chunks {:?}",s);
+    }
+}
